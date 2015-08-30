f92e5ac44b9ca08204b122ff8096e35178aaa4d6
HassanS6000 <hassansyyid@gmail.com>
2015-08-30 13:44:43 -0400

    Updated /broadcast. It now supports color codes!

--- src/main/java/io/github/hsyyid/spongeessentialcmds/cmdexecutors/BroadcastExecutor.java
+++ src/main/java/io/github/hsyyid/spongeessentialcmds/cmdexecutors/BroadcastExecutor.java
@@ -5,8 +5,10 @@ import io.github.hsyyid.spongeessentialcmds.Main;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.util.TextMessageException;
 import org.spongepowered.api.util.command.CommandException;
 import org.spongepowered.api.util.command.CommandResult;
 import org.spongepowered.api.util.command.CommandSource;
@@ -15,15 +17,36 @@ import org.spongepowered.api.util.command.spec.CommandExecutor;
 
 public class BroadcastExecutor implements CommandExecutor
 {
+	@SuppressWarnings("deprecation")
 	public CommandResult execute(CommandSource src, CommandContext ctx) throws CommandException
 	{
 		String message = ctx.<String>getOne("message").get();
 		Game game = Main.game;
 		Server server = game.getServer();
-		for(Player player : server.getOnlinePlayers())
+		
+		try 
 		{
-			player.sendMessage(Texts.of(TextColors.DARK_GRAY, "[",TextColors.DARK_RED,"Broadcast", TextColors.DARK_GRAY, "]", TextColors.GREEN, " " + message));
+			Text msg = Texts.legacy('&').from(message);
+			Text broadcast = Texts.of(TextColors.DARK_GRAY, "[",TextColors.DARK_RED,"Broadcast", TextColors.DARK_GRAY, "]", TextColors.GREEN, " ");
+			Text finalBroadcast = Texts.builder().append(broadcast).append(msg).build();
+			
+			for(Player player : server.getOnlinePlayers())
+			{
+				player.sendMessage(finalBroadcast);
+			}
+			
+			server.getConsole().sendMessage(finalBroadcast);
 		}
+		catch (TextMessageException e)
+		{
+			for(Player player : server.getOnlinePlayers())
+			{
+				player.sendMessage(Texts.of(TextColors.DARK_GRAY, "[",TextColors.DARK_RED,"Broadcast", TextColors.DARK_GRAY, "]", TextColors.GREEN, " " + message));
+			}
+			
+			server.getConsole().sendMessage(Texts.of(TextColors.DARK_GRAY, "[",TextColors.DARK_RED,"Broadcast", TextColors.DARK_GRAY, "]", TextColors.GREEN, " " + message));
+		}
+		
 		return CommandResult.success();
 	}
 }
