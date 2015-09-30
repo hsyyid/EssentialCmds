7c5eea862b296f1c5b1f350ccca4f278e6c6bee1
HassanS6000 <hassansyyid@gmail.com>
2015-09-30 15:12:42 -0400

    HouseKeeping

--- src/main/java/io/github/hsyyid/spongeessentialcmds/cmdexecutors/HatExecutor.java
+++ src/main/java/io/github/hsyyid/spongeessentialcmds/cmdexecutors/HatExecutor.java
@@ -10,26 +10,34 @@ import org.spongepowered.api.util.command.CommandSource;
 import org.spongepowered.api.util.command.args.CommandContext;
 import org.spongepowered.api.util.command.spec.CommandExecutor;
 
-public class HatExecutor implements CommandExecutor {
-
+public class HatExecutor implements CommandExecutor
+{
 	@Override
-	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-		if(src instanceof Player) {
+	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException
+	{
+		if (src instanceof Player)
+		{
 			Player player = (Player) src;
 			Optional<ItemStack> itemInHand = player.getItemInHand();
-			if (itemInHand.isPresent()) {
+			if (itemInHand.isPresent())
+			{
 				player.setHelmet(itemInHand.get());
 				itemInHand.get().setQuantity(itemInHand.get().getQuantity() - 1);
-				if(itemInHand.get().getQuantity() > 0)//Sponge derped up itemstack verification
+				if (itemInHand.get().getQuantity() > 0)
+				{
 					player.setItemInHand(itemInHand.get());
+				}
 				else
+				{
 					player.setItemInHand(null);
-			} else {
+				}
+			}
+			else
+			{
 				player.sendMessage(Texts.of("No item selected in hotbar."));
 				return CommandResult.empty();
 			}
 		}
 		return CommandResult.success();
 	}
-
 }
--- src/main/java/io/github/hsyyid/spongeessentialcmds/cmdexecutors/LightningExecutor.java
+++ src/main/java/io/github/hsyyid/spongeessentialcmds/cmdexecutors/LightningExecutor.java
@@ -1,5 +1,7 @@
 package io.github.hsyyid.spongeessentialcmds.cmdexecutors;
 
+import org.spongepowered.api.event.cause.Cause;
+
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
@@ -15,43 +17,37 @@ import org.spongepowered.api.util.command.spec.CommandExecutor;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.extent.Extent;
-
 import com.google.common.base.Optional;
 
 public class LightningExecutor implements CommandExecutor
 {
-    public CommandResult execute(CommandSource src, CommandContext ctx) throws CommandException
-    {
-        if(src instanceof Player)
-        {
-            Player player = (Player) src;
-            Location<World> playerLocation = player.getLocation();
-            Location<World> lightningLocation = new Location<World>(playerLocation.getExtent(), playerLocation.getX() + 5, playerLocation.getY(), playerLocation.getZ());
-            spawnEntity(lightningLocation);
-            player.sendMessage(Texts.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Created Lightning Strike!"));
-        }
-        else if(src instanceof ConsoleSource) {
-            src.sendMessage(Texts.of(TextColors.DARK_RED,"Error! ", TextColors.RED, "Must be an in-game player to use /lightning!"));
-        }
-        else if(src instanceof CommandBlockSource) {
-            src.sendMessage(Texts.of(TextColors.DARK_RED,"Error! ", TextColors.RED, "Must be an in-game player to use /lightning!"));
-        }
-        return CommandResult.success();
-    }
-    
-    public void spawnEntity(Location<World> location)
-    {
-        Extent extent = location.getExtent();
-        Optional<Entity> optional = extent.createEntity(EntityTypes.LIGHTNING, location.getPosition());
+	public CommandResult execute(CommandSource src, CommandContext ctx) throws CommandException
+	{
+		if (src instanceof Player)
+		{
+			Player player = (Player) src;
+			Location<World> playerLocation = player.getLocation();
+			Location<World> lightningLocation = new Location<World>(playerLocation.getExtent(), playerLocation.getX(), playerLocation.getY() + 50, playerLocation.getZ());
+			spawnEntity(lightningLocation);
+			player.sendMessage(Texts.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Created Lightning Strike!"));
+		}
+		else if (src instanceof ConsoleSource)
+		{
+			src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Must be an in-game player to use /lightning!"));
+		}
+		else if (src instanceof CommandBlockSource)
+		{
+			src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Must be an in-game player to use /lightning!"));
+		}
+		return CommandResult.success();
+	}
 
-        if (optional.isPresent())
-        {
-            Entity lightning = optional.get();
-            System.out.println("Spawned Lightning " + extent.spawnEntity(lightning, null));
-        }
-        else
-        {
-            System.out.println("Error! Optional Entity is null");
-        }
-    }
+	public void spawnEntity(Location<World> location)
+	{
+		Extent extent = location.getExtent();
+		Optional<Entity> optional = extent.createEntity(EntityTypes.LIGHTNING, location.getPosition());
+
+		Entity lightning = optional.get();
+		System.out.println("Spawned Lightning " + extent.spawnEntity(lightning, Cause.empty()));
+	}
 }
