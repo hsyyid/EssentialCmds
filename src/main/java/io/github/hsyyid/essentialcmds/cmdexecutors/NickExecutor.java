e9d31fc5e7f5f485edbcf5f7a4ea2f2f62805937
Hassan Syyid <hassansyyid@gmail.com>
2015-11-29 16:07:17 -0500

    Fix issues with perms

--- src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/NickExecutor.java
+++ src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/NickExecutor.java
@@ -11,61 +11,46 @@ import org.spongepowered.api.util.command.CommandSource;
 import org.spongepowered.api.util.command.args.CommandContext;
 import org.spongepowered.api.util.command.spec.CommandExecutor;
 
-import java.util.Optional;
-
 public class NickExecutor implements CommandExecutor
 {
 	public CommandResult execute(CommandSource src, CommandContext ctx) throws CommandException
 	{
-		Optional<Player> target = ctx.getOne("player");
-
+		Player player = ctx.<Player> getOne("player").get();
 		String nick = ctx.<String> getOne("nick").get();
 		nick = nick.replace("&", "\u00A7");
 
-		if (!target.isPresent())
+		if (src instanceof Player && ((Player) src).getUniqueId() != player.getUniqueId() && src.hasPermission("essentialcmds.nick.others"))
 		{
-			if (src instanceof Player)
+			Subject subject = player.getContainingCollection().get(player.getIdentifier());
+
+			if (subject instanceof OptionSubject)
 			{
-				Player player = (Player) src;
-
-				Subject subject = player.getContainingCollection().get(player.getIdentifier());
-
-				if (subject instanceof OptionSubject)
-				{
-					OptionSubject optionSubject = (OptionSubject) subject;
-					optionSubject.getTransientSubjectData().setOption(player.getActiveContexts(), "nick", nick);
-					src.sendMessage(Texts.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Nick successfully set!"));
-				}
-				else
-				{
-					src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Your server doesn't currently support /nick!"));
-				}
+				OptionSubject optionSubject = (OptionSubject) subject;
+				optionSubject.getTransientSubjectData().setOption(player.getActiveContexts(), "nick", nick);
+				src.sendMessage(Texts.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Nick successfully set!"));
 			}
 			else
 			{
-				src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "You must be a in-game player to do /nick!"));
+				src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Your server doesn't currently support /nick!"));
 			}
+		}
+		else if(src instanceof Player && ((Player) src).getUniqueId() != player.getUniqueId())
+		{
+			src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "You do not have permission to make changes to other player's nicknames.!"));
 		}
 		else
 		{
-			if (src.hasPermission("essentialcmds.nick.others"))
-			{
-				Player player = target.get();
-				Subject subject = player.getContainingCollection().get(player.getIdentifier());
+			Subject subject = player.getContainingCollection().get(player.getIdentifier());
 
-				if (subject instanceof OptionSubject)
-				{
-					OptionSubject optionSubject = (OptionSubject) subject;
-					optionSubject.getTransientSubjectData().setOption(player.getActiveContexts(), "nick", nick);
-				}
-				else
-				{
-					src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Your server doesn't currently support /nick!"));
-				}
+			if (subject instanceof OptionSubject)
+			{
+				OptionSubject optionSubject = (OptionSubject) subject;
+				optionSubject.getTransientSubjectData().setOption(player.getActiveContexts(), "nick", nick);
+				src.sendMessage(Texts.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Nick successfully set!"));
 			}
 			else
 			{
-				src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "You do not have permission to make changes to other player's nicknames.!"));
+				src.sendMessage(Texts.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Your server doesn't currently support /nick!"));
 			}
 		}
 
