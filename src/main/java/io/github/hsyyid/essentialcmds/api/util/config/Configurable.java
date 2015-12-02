c357ada873833aa752f9c99006d6fc553bb01dce
The-King <hunar911@gmail.com>
2015-12-02 09:31:15 -0800

    added Configurable interface.

---
+++ src/main/java/io/github/hsyyid/essentialcmds/api/util/config/Configurable.java
@@ -0,0 +1,17 @@
+package io.github.hsyyid.essentialcmds.api.util.config;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+
+/**
+ * Template used for all config files
+ */
+public interface Configurable {
+
+    void setup();
+
+    void load();
+
+    void save();
+
+    CommentedConfigurationNode get();
+}
