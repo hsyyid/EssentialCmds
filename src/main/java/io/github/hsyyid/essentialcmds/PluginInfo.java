bcd08a7f910dcc0e7bff179ff2e69ddacbefd793
The-King <hunar911@gmail.com>
2015-12-01 20:03:25 -0800

    added PluginInfo

---
+++ src/main/java/io/github/hsyyid/essentialcmds/PluginInfo.java
@@ -0,0 +1,12 @@
+package io.github.hsyyid.essentialcmds;
+
+/**
+ * General plugin info
+ */
+public abstract class PluginInfo {
+
+    public static final String ID = "EssentialCmds";
+    public static final String NAME = "EssentialCmds";
+    public static final String VERSION = "5.6";
+    public static final String DEPENDENCIES = "";
+}
