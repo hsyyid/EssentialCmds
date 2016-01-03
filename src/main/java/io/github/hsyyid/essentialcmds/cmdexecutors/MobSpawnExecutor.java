7a3c0594247fa450d7fd9592e4eacc23668d6728
Hassan Syyid <hassansyyid@gmail.com>
2016-01-03 12:53:30 -0500

    MobSpawn and MobSpawner support Modded Mobs.

--- src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/MobSpawnExecutor.java
+++ src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/MobSpawnExecutor.java
@@ -24,6 +24,7 @@
  */
 package io.github.hsyyid.essentialcmds.cmdexecutors;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
@@ -34,7 +35,6 @@ import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityType;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.text.Text;
@@ -58,109 +58,12 @@ public class MobSpawnExecutor implements CommandExecutor
 		{
 			Player player = (Player) src;
 
-			EntityType type = null;
+			EntityType type = Sponge.getRegistry().getType(EntityType.class, entityType).orElse(null);
 
-			switch (entityType.toLowerCase())
+			if (type == null)
 			{
-				case "bat":
-					type = EntityTypes.BAT;
-					break;
-				case "blaze":
-					type = EntityTypes.BLAZE;
-					break;
-				case "cave spider":
-					type = EntityTypes.CAVE_SPIDER;
-					break;
-				case "chicken":
-					type = EntityTypes.CHICKEN;
-					break;
-				case "cow":
-					type = EntityTypes.COW;
-					break;
-				case "creeper":
-					type = EntityTypes.CREEPER;
-					break;
-				case "ender dragon":
-					type = EntityTypes.ENDER_DRAGON;
-					break;
-				case "enderman":
-					type = EntityTypes.ENDERMAN;
-					break;
-				case "endermite":
-					type = EntityTypes.ENDERMITE;
-					break;
-				case "ghast":
-					type = EntityTypes.GHAST;
-					break;
-				case "giant":
-					type = EntityTypes.GIANT;
-					break;
-				case "guardian":
-					type = EntityTypes.GUARDIAN;
-					break;
-				case "horse":
-					type = EntityTypes.HORSE;
-					break;
-				case "iron golem":
-					type = EntityTypes.IRON_GOLEM;
-					break;
-				case "magma cube":
-					type = EntityTypes.MAGMA_CUBE;
-					break;
-				case "mushroom cow":
-					type = EntityTypes.MUSHROOM_COW;
-					break;
-				case "ocelot":
-					type = EntityTypes.OCELOT;
-					break;
-				case "pig":
-					type = EntityTypes.PIG;
-					break;
-				case "pig zombie":
-					type = EntityTypes.PIG_ZOMBIE;
-					break;
-				case "rabbit":
-					type = EntityTypes.RABBIT;
-					break;
-				case "sheep":
-					type = EntityTypes.SHEEP;
-					break;
-				case "silverfish":
-					type = EntityTypes.SILVERFISH;
-					break;
-				case "skeleton":
-					type = EntityTypes.SKELETON;
-					break;
-				case "slime":
-					type = EntityTypes.SLIME;
-					break;
-				case "snowman":
-					type = EntityTypes.SNOWMAN;
-					break;
-				case "spider":
-					type = EntityTypes.SPIDER;
-					break;
-				case "squid":
-					type = EntityTypes.SQUID;
-					break;
-				case "villager":
-					type = EntityTypes.VILLAGER;
-					break;
-				case "witch":
-					type = EntityTypes.WITCH;
-					break;
-				case "wither":
-					type = EntityTypes.WITHER;
-					break;
-				case "wolf":
-					type = EntityTypes.WOLF;
-					break;
-				case "zombie":
-					type = EntityTypes.ZOMBIE;
-					break;
-				default:
-					src.sendMessage(Text.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "The mob you inputed was not recognized."));
-					return CommandResult.success();
+				src.sendMessage(Text.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "The mob you inputed was not recognized."));
+				return CommandResult.success();
 			}
 
 			spawnEntity(getSpawnLocFromPlayerLoc(player).add(0, 1, 0), type, amount);
--- src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/MobSpawnerExecutor.java
+++ src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/MobSpawnerExecutor.java
@@ -25,6 +25,7 @@
 package io.github.hsyyid.essentialcmds.cmdexecutors;
 
 import io.github.hsyyid.essentialcmds.EssentialCmds;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -34,7 +35,6 @@ import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.data.manipulator.mutable.MobSpawnerData;
 import org.spongepowered.api.entity.EntityType;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
@@ -52,126 +52,29 @@ public class MobSpawnerExecutor implements CommandExecutor
 		if (src instanceof Player)
 		{
 			Player player = (Player) src;
-			EntityType type = null;
-
-			switch (entityType.toLowerCase())
+			EntityType type = Sponge.getRegistry().getType(EntityType.class, entityType).orElse(null);
+			if (type != null)
 			{
-				case "bat":
-					type = EntityTypes.BAT;
-					break;
-				case "blaze":
-					type = EntityTypes.BLAZE;
-					break;
-				case "cave spider":
-					type = EntityTypes.CAVE_SPIDER;
-					break;
-				case "chicken":
-					type = EntityTypes.CHICKEN;
-					break;
-				case "cow":
-					type = EntityTypes.COW;
-					break;
-				case "creeper":
-					type = EntityTypes.CREEPER;
-					break;
-				case "ender dragon":
-					type = EntityTypes.ENDER_DRAGON;
-					break;
-				case "enderman":
-					type = EntityTypes.ENDERMAN;
-					break;
-				case "endermite":
-					type = EntityTypes.ENDERMITE;
-					break;
-				case "ghast":
-					type = EntityTypes.GHAST;
-					break;
-				case "giant":
-					type = EntityTypes.GIANT;
-					break;
-				case "guardian":
-					type = EntityTypes.GUARDIAN;
-					break;
-				case "horse":
-					type = EntityTypes.HORSE;
-					break;
-				case "iron golem":
-					type = EntityTypes.IRON_GOLEM;
-					break;
-				case "magma cube":
-					type = EntityTypes.MAGMA_CUBE;
-					break;
-				case "mushroom cow":
-					type = EntityTypes.MUSHROOM_COW;
-					break;
-				case "ocelot":
-					type = EntityTypes.OCELOT;
-					break;
-				case "pig":
-					type = EntityTypes.PIG;
-					break;
-				case "pig zombie":
-					type = EntityTypes.PIG_ZOMBIE;
-					break;
-				case "rabbit":
-					type = EntityTypes.RABBIT;
-					break;
-				case "sheep":
-					type = EntityTypes.SHEEP;
-					break;
-				case "silverfish":
-					type = EntityTypes.SILVERFISH;
-					break;
-				case "skeleton":
-					type = EntityTypes.SKELETON;
-					break;
-				case "slime":
-					type = EntityTypes.SLIME;
-					break;
-				case "snowman":
-					type = EntityTypes.SNOWMAN;
-					break;
-				case "spider":
-					type = EntityTypes.SPIDER;
-					break;
-				case "squid":
-					type = EntityTypes.SQUID;
-					break;
-				case "villager":
-					type = EntityTypes.VILLAGER;
-					break;
-				case "witch":
-					type = EntityTypes.WITCH;
-					break;
-				case "wither":
-					type = EntityTypes.WITHER;
-					break;
-				case "wolf":
-					type = EntityTypes.WOLF;
-					break;
-				case "zombie":
-					type = EntityTypes.ZOMBIE;
-					break;
-				default:
-					src.sendMessage(Text.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "The mob you inputed was not recognized."));
-					return CommandResult.success();
-			}
+				ItemStack.Builder itemBuilder = EssentialCmds.getEssentialCmds().getGame().getRegistry().createBuilder(ItemStack.Builder.class);
+				ItemStack mobSpawnerStack = itemBuilder.itemType(ItemTypes.MOB_SPAWNER).quantity(1).build();
+				Optional<MobSpawnerData> optionalMobSpawnerData = mobSpawnerStack.getOrCreate(MobSpawnerData.class);
 
-			ItemStack.Builder itemBuilder = EssentialCmds.getEssentialCmds().getGame().getRegistry().createBuilder(ItemStack.Builder.class);
-			ItemStack mobSpawnerStack = itemBuilder.itemType(ItemTypes.MOB_SPAWNER).quantity(1).build();
-			Optional<MobSpawnerData> optionalMobSpawnerData = mobSpawnerStack.getOrCreate(MobSpawnerData.class);
-
-			if (optionalMobSpawnerData.isPresent())
-			{
-				MobSpawnerData mobSpawnerData = optionalMobSpawnerData.get();
-				mobSpawnerData = mobSpawnerData.set(mobSpawnerData.nextEntityToSpawn().set(type, null));
-				mobSpawnerStack.offer(mobSpawnerData);
-				player.setItemInHand(mobSpawnerStack);
-				player.sendMessage(Text.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Spawned mob spawner."));
+				if (optionalMobSpawnerData.isPresent())
+				{
+					MobSpawnerData mobSpawnerData = optionalMobSpawnerData.get();
+					mobSpawnerData = mobSpawnerData.set(mobSpawnerData.nextEntityToSpawn().set(type, null));
+					mobSpawnerStack.offer(mobSpawnerData);
+					player.setItemInHand(mobSpawnerStack);
+					player.sendMessage(Text.of(TextColors.GREEN, "Success! ", TextColors.YELLOW, "Spawned mob spawner."));
+				}
+				else
+				{
+					player.sendMessage(Text.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Could not apply mob spawner data."));
+				}
 			}
 			else
 			{
-				player.sendMessage(Text.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "Could not apply mob spawner data."));
+				player.sendMessage(Text.of(TextColors.DARK_RED, "Error! ", TextColors.RED, "EntityType not recognized."));
 			}
 		}
 		else if (src instanceof ConsoleSource)
