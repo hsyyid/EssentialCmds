1c073d8956a40a2ec7753cb9ba375d0360148632
kinggoesgaming <hunar.roop@gmail.com>
2016-02-07 17:49:04 -0800

    made the token for git version more informative

--- src/main/java/io/github/hsyyid/essentialcmds/PluginInfo.java
+++ src/main/java/io/github/hsyyid/essentialcmds/PluginInfo.java
@@ -33,5 +33,5 @@ public abstract class PluginInfo
 	public static final String NAME = "EssentialCmds";
 	public static final String VERSION = "@project.version@";
 	public static final String DEPENDENCIES = "after:Updatifier";
-	public static final String INFORMATIVE_VERSION = "@project.gitVersion@";
+	public static final String INFORMATIVE_VERSION = "@project.informativeVersion@";
 }
