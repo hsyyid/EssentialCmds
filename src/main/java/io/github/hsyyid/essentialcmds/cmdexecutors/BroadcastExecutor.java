cb3d34ce45d89e2ba1be72c875197d8251baa057
Jaggar <gigiaboone@yahoo.com>
2016-04-08 13:19:00 -0400

    Update BroadcastExecutor.java

--- src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/BroadcastExecutor.java
+++ src/main/java/io/github/hsyyid/essentialcmds/cmdexecutors/BroadcastExecutor.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of EssentialCmds, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2015 - 2016 HassanS6000
+ * Copyright (c) 2015 - 2015 HassanS6000
  * Copyright (c) contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
