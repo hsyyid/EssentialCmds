87caf78a1fdd9ec3551ea5ba0db0473eb1f4d8f0
Hassan Syyid <hassansyyid@gmail.com>
2016-05-12 14:10:53 -0400

    Minor fixes

--- src/main/java/io/github/hsyyid/essentialcmds/utils/EssLogger.java
+++ src/main/java/io/github/hsyyid/essentialcmds/utils/EssLogger.java
@@ -30,21 +30,21 @@ import org.spongepowered.api.Sponge;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.serializer.TextSerializers;
 
-public class EssLogger {
-
-	public static Text toText(String str){
-    	return TextSerializers.FORMATTING_CODE.deserialize(str);
-    }
-	
-	
-	//info
-	public void info(String string) {
-		Sponge.getServer().getConsole().sendMessage(toText("["+PluginInfo.NAME+"]: "+string));
+public class EssLogger
+{
+	public static Text toText(String str)
+	{
+		return TextSerializers.FORMATTING_CODE.deserialize(str);
 	}
-	
-	//error
-	public void error(String string) {
-		Sponge.getServer().getConsole().sendMessage(toText("["+PluginInfo.NAME+"]: &c"+string));
+
+	public void info(String string)
+	{
+		Sponge.getServer().getConsole().sendMessage(toText("[" + PluginInfo.NAME + "]: " + string));
+	}
+
+	public void error(String string)
+	{
+		Sponge.getServer().getConsole().sendMessage(toText("[" + PluginInfo.NAME + "]: &c" + string));
 	}
 
 }
